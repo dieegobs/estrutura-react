b4a97f376217cb3651109488bb7926091c45958c
dieegobs <diegohevant@gmail.com>
2023-01-31 23:37:30 -0300

    Componentização passando props

--- src/app/pages/login/components/InputLogin.tsx
+++ src/app/pages/login/components/InputLogin.tsx
@@ -1,8 +1,12 @@
-export const InputLogin = () => {
-    return(
+interface IInputLoginProps {
+    label: string;
+}
+
+export const InputLogin: React.FC<IInputLoginProps> = (props) => {
+    return (
         <label htmlFor="">
-                    <span>Email</span>
-                    <input type="text" />
-                </label>
+            <span>props.label</span>
+            <input type="text" />
+        </label>
     )
 }
